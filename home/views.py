f46d76a8a03e46d054d873ee01745c411ef69b80
Mithun Raj <kmithunraj02@gmail.com>
2023-01-28 12:53:30 +0530

    authentication check

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
-
+@login_required(login_url="/login/")
 def homepg(request):
     return render(request, 'home.html')
+
+
+@login_required
+def logoutpg(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("")
